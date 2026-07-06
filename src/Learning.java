2cf69c98b352a23893275b84780922470c2bf6e6
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-07 00:23:57 +0530

    Modified website assets

--- src/Learning.java
+++ src/Learning.java
@@ -1,3 +1,8 @@
 public class Learning {
-    
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.print("123");
+        System.out.println(123.165);
+
+    }
 }
