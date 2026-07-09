08d7ae35740a66c46e224a41ebf283ad0d66f6f3
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 12:21:59 +0530

    Updated portfolio content

--- src/ScannerPractice.java
+++ src/ScannerPractice.java
@@ -1,16 +1,42 @@
 import java.util.Scanner;
 
 public class ScannerPractice {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("What is your age?");
-        boolean x = scanner.nextBoolean();
-        System.out.println(x);
-        scanner.close();
-
-        void areaOfRectangle {
-
+    
+    static void areraOfRectangle() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter length of Rectangle");
+            double length = scanner.nextDouble();
+            System.out.println("Enter the breath of Rectangle");
+            double breath = scanner.nextDouble();
+            double area = length * breath;
+            System.out.println("Area of triangle is " + area);
+            scanner.close();20
         }
+    static void areraOfTriangle() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter length of Traingle");
+            double length = scanner.nextDouble();
+            System.out.println("Enter the height of Triangle");
+            double breath = scanner.nextDouble();
+            double area = 0.5 * length * breath;
+            System.out.println("Area of triangle is " + area);
+            scanner.close();
+        }
+    static void simpleIntrest() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Principal Amount");
+            double principal = scanner.nextDouble();
+            System.out.println("Enter intrest rate");
+            double intrest = scanner.nextDouble();
+            System.out.println("Enter time in year");
+            double time = scanner.nextDouble();
+            double si = (principal * intrest * time)/100;
+            System.out.println("Simple Intrest is " + si);
+            scanner.close();
+        }
+    public static void main(String[] args) {
+        // areraOfRectangle();
+        // areraOfTriangle();
+        
     }
 }
