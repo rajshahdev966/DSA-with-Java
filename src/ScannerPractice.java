3fbbdd7021cec36e83132f7161f6d5cbf0683136
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 12:23:41 +0530

    Modified website assets

--- src/ScannerPractice.java
+++ src/ScannerPractice.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ScannerPractice {
-    
+
     static void areraOfRectangle() {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter length of Rectangle");
@@ -12,31 +12,34 @@ public class ScannerPractice {
             System.out.println("Area of triangle is " + area);
             scanner.close();20
         }
+
     static void areraOfTriangle() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter length of Traingle");
-            double length = scanner.nextDouble();
-            System.out.println("Enter the height of Triangle");
-            double breath = scanner.nextDouble();
-            double area = 0.5 * length * breath;
-            System.out.println("Area of triangle is " + area);
-            scanner.close();
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter length of Traingle");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the height of Triangle");
+        double breath = scanner.nextDouble();
+        double area = 0.5 * length * breath;
+        System.out.println("Area of triangle is " + area);
+        scanner.close();
+    }
+
     static void simpleIntrest() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter Principal Amount");
-            double principal = scanner.nextDouble();
-            System.out.println("Enter intrest rate");
-            double intrest = scanner.nextDouble();
-            System.out.println("Enter time in year");
-            double time = scanner.nextDouble();
-            double si = (principal * intrest * time)/100;
-            System.out.println("Simple Intrest is " + si);
-            scanner.close();
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Principal Amount");
+        double principal = scanner.nextDouble();
+        System.out.println("Enter intrest rate");
+        double intrest = scanner.nextDouble();
+        System.out.println("Enter time in year");
+        double time = scanner.nextDouble();
+        double si = (principal * intrest * time) / 100;
+        System.out.println("Simple Intrest is " + si);
+        scanner.close();
+    }
+
     public static void main(String[] args) {
         // areraOfRectangle();
         // areraOfTriangle();
-        
+
     }
 }
