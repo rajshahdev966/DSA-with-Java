670656c86d9457c31b3c30d9a5099aaa7f5d392f
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 08:47:40 +0530

    Updated portfolio content

--- src/escapeSequence.java
+++ src/escapeSequence.java
@@ -3,12 +3,8 @@ public class escapeSequence {
     // System.out.println("Hello Welcome to Raj Shah \nDo you understand ");
     // System.out.println("Hello Welcome to Raj Shah \"Do you understand\" ");
     // System.out.println("Hello \'How are you doing?");  
-    
-        System.out.println("  *");
-        System.out.println(" **");
-        System.out.println("***");
-        System.out.println(" **");
-        System.out.println("  *");
+    float inch = 61.8f;
+
 
 }
 }
