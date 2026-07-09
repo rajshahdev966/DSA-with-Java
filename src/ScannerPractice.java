f227d76f706ca63acad996a4406696edb88799a8
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 09:20:04 +0530

    Update project files

--- src/ScannerPractice.java
+++ src/ScannerPractice.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class ScannerPractice {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        float x = scanner.nextFloat();
+        System.out.println(x);
+
     }
 }
