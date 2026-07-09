6b05cffc83f7ca02fc50ba752e9073c8c23b3dba
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-09 09:24:57 +0530

    Update project files

--- src/ScannerPractice.java
+++ src/ScannerPractice.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ScannerPractice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float x = scanner.nextFloat();
-        System.out.println("What is your age?");
-        System.out.println(x);
 
+        System.out.println("What is your age?");
+        float x = scanner.nextFloat();
+        System.out.println(x);
     }
 }
